6a69be97d4a81fbacd65bdb7ac31934d54a93429
abduse1 <abduselamed12@gmail.com>
2025-01-31 22:01:34 +0530

    f-string used to print in variable.py

--- Python-Basics/variable.py
+++ Python-Basics/variable.py
@@ -1,5 +1,9 @@
 patient_name = 'Abraham'
 age = 23
 height = 1.76
-weight = 3
+weight = 63
 print(patient_name, age, height, weight)
+
+tb_Postive = True
+
+print(f"The patient name is {patient_name} and he is {age} years old and Is he a TB Patient? {tb_Postive}")
