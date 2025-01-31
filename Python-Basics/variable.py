a962674466c34255b3e65523cab40a9274d04b87
abduse1 <abduselamed12@gmail.com>
2025-01-31 22:09:05 +0530

    improved varible.py

--- Python-Basics/variable.py
+++ Python-Basics/variable.py
@@ -4,6 +4,6 @@ height = 1.76
 weight = 63
 print(patient_name, age, height, weight)
 
-tb_Postive = True
+tb_postive = True
 
 print(f"The patient name is {patient_name} and he is {age} years old and Is he a TB Patient? {tb_Postive}")
